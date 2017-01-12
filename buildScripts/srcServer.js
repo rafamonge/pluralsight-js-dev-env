1425dbf4053dfb6c19274b069436a2328268b263
Rafael Monge Montenegro <rafamonge@gmail.com>
2017-01-12 12:31:02 -0600

    checkin

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,6 +15,17 @@ app.use(require('webpack-dev-middleware')(compiler,{
     publicPath: config.output.publicPath
 }));
 
+app.get('/users', function(req, res){
+    res.json(
+        [
+            {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
+            {"id": 2, "firstName": "Tammy", "lastName": "Smith", "email": "bob@gmail.com" },
+            {"id": 3, "firstName": "Tiana", "lastName": "Smith", "email": "bob@gmail.com" }
+        ]
+    );
+});
+
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import numeral from 'numeral';
 import './index.css';
 const courseValue = numeral(100).format('$0,0.00');
-console.log(` pay ${courseValue} for this course!!!!`);
+console.log(` pay ${courseValue} for this course!!!!`); // eslint-disable-line no-console
